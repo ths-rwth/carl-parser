59e9bfed16d244ceed4894d92ad81b0ae1e723a9
Gereon Kremer <gereon.kremer@cs.rwth-aachen.de>
2017-07-13 08:56:33 +0200

    Working on travis build, pulling carl if necessary.

--- .ci/build_travis.sh
+++ .ci/build_travis.sh
@@ -2,19 +2,28 @@
 
 pushd .
 
-git clone https://github.com/smtrat/carl.git
-mkdir -p carl/build
-cd carl/build/
+if [ ! -d carl/ ] ; then
+	git clone https://github.com/smtrat/carl.git
+else
+	pushd carl/
+	git fetch
+	git reset --hard origin/master
+	popd
+fi
 
+mkdir -p carl/build
+pushd carl/build/
 cmake -D DEVELOPER=ON -D BUILD_ADDONS=OFF ../ || return 1
 make lib_carl || return 1
-
 popd
+
 
 mkdir -p build
 cd build/
 
-cmake ../
-make
+cmake ../ || return 1
+make || return 1
 
 ./test/carl-parser-test
+
+popd
