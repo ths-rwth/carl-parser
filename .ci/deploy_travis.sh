6cea719bb9a7010394a5f467701423ef66da75d7
Gereon Kremer <gereon.kremer@rwth-aachen.de>
2017-10-13 15:21:10 +0200

    Debugging purging of deploy branches.

--- .ci/deploy_travis.sh
+++ .ci/deploy_travis.sh
@@ -15,31 +15,35 @@ if [[ ${TASK} == "DEPLOY" ]]; then
 
 	git remote add github https://${GH_TOKEN}@github.com/smtrat/carl-parser.git
 	git push github $BRANCH --tags --force
+	
+	git for-each-ref --sort=committerdate --format='%(refname:short)'
+
+	git fetch --unshallow
+	
+	git for-each-ref --sort=committerdate --format='%(refname:short)'
+
+	for branch in `git for-each-ref --sort=committerdate --format='%(refname:short)'`
+	do
+		# Avoid deleting branches other than deploy-<commitid>
+		if [[ ! "$branch" =~ ^origin/deploy-[0-9a-f]+$ ]]; then
+			continue
+		fi
+		
+		# Avoid deleting branches referenced by tags
+		tags=`git log --oneline --decorate $branch | grep "tag:"`
+		if [[ ! -z "$tags" ]]; then
+			continue
+		fi
+		
+		# Avoid deleting branches less than a week old
+		now=`date +%s`
+		lastcommit=`git log --pretty=format:'%at' -1 $branch`
+		days=$(((now-lastcommit)/3600/24))
+		if [ "$days" -lt 7 ]; then
+			continue
+		fi
+		
+		echo "Deleting $branch..."
+		git push origin --delete $branch
+	done
 fi
-
-git fetch --unshallow
-
-for branch in `git for-each-ref --sort=committerdate --format='%(refname:short)'`
-do
-	# Avoid deleting branches other than deploy-<commitid>
-	if [[ ! "$branch" =~ ^origin/deploy-[0-9a-f]+$ ]]; then
-		continue
-	fi
-	
-	# Avoid deleting branches referenced by tags
-	tags=`git log --oneline --decorate $branch | grep "tag:"`
-	if [[ ! -z "$tags" ]]; then
-		continue
-	fi
-	
-	# Avoid deleting branches less than a week old
-	now=`date +%s`
-	lastcommit=`git log --pretty=format:'%at' -1 $branch`
-	days=$(((now-lastcommit)/3600/24))
-	if [ "$days" -lt 7 ]; then
-		continue
-	fi
-	
-	echo "Deleting $branch..."
-	git push origin --delete $branch
-done
